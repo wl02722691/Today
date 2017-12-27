67c2a1fdebe7826d4acc13c886db728f0c485920
Alice <wl02722691@gmail.com>
2017-12-27 13:06:04 +0800

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by 張書涵 on 2017/12/27.
+//  Copyright © 2017年 AliceChang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

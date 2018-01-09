d5c1b1cc8e3fda4f97ba561140dfc8bcb858ff82
Alice <wl02722691@gmail.com>
2018-01-09 21:12:19 +0800

    Persisting Data using Core Data

--- Today/AppDelegate.swift
+++ Today/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,9 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        //print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
         return true
-        //比viewDidLoad還要更早讀取
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -35,10 +34,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
       }
 
+   
     func applicationWillTerminate(_ application: UIApplication) {
-        print("applicationWillTerminate")
-        //把App從緩存中刪除的狀況
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Saves changes in the application's managed object context before the application terminates.
+        self.saveContext()
     }
-
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+ 
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
 }
 
--- Today/Controllers/TodoListViewController.swift
+++ Today/Controllers/TodoListViewController.swift
@@ -6,25 +6,34 @@
 //  Copyright © 2017年 AliceChang. All rights reserved.
 //
 
+import CoreData
 import UIKit
 
-class TodoListViewController: UITableViewController {
-
+class TodoListViewController: UITableViewController{
     var itemArray = [Item]()
+    
+    var selectedCategory : Category? {
+        didSet{
+            loadItem()
+        }
+    }
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+   
 
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
 
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        loadItem()
-        
+    super.viewDidLoad()
+    //  print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+    
+
 //
 //        if var item = defaults.array(forKey: "TodoListArray") as? [Item]{
 //           itemArray = item
 //        }
     }
+
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -32,7 +41,6 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        
        let item = itemArray[indexPath.row]
         cell.textLabel?.text = item.title
         
@@ -53,6 +61,10 @@ class TodoListViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+       // context.delete(itemArray[indexPath.row])
+       // itemArray.remove(at: indexPath.row)
+        
+        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         saveItem()
@@ -64,7 +76,7 @@ class TodoListViewController: UITableViewController {
 //            itemArray[indexPath.row].done = true
 //        }
         
-        print(itemArray[indexPath.row])//print出itemArray的字
+       // print(itemArray[indexPath.row])//print出itemArray的字
       
         
         tableView.deselectRow(at: indexPath, animated: true) //讓原先被選到的那列變深色的效果消失
@@ -81,9 +93,12 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction.init(title: "Add New Item", style: .default) { (action) in
             //what wll happen once the user clicks the Add Item button on our UIAlert
-          
-            let newItem = Item()
+            
+            //要用Application不能直接用AppDelegate.persistentContainer.viewContext因為AppDelegate是class要跨檔案使用要delegate
+            let newItem = Item(context: self.context)
+            newItem.done = false
             newItem.title = textField.text!
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             self.saveItem()
 
@@ -101,28 +116,98 @@ class TodoListViewController: UITableViewController {
     }
 
     func saveItem(){
-        let encoder = PropertyListEncoder()
+        
         do{
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         }catch{
-            print("Error encoding item  array,\(error.localizedDescription)")
+            print("error saving context\(error.localizedDescription)")
         }
         
         self.tableView.reloadData()
     }
     
-    func loadItem(){
-        if let data = try? Data(contentsOf: dataFilePath!){
-        let decoder = PropertyListDecoder()
+    func loadItem(with request:NSFetchRequest<Item> = Item.fetchRequest(), predicate:NSPredicate? = nil){
+        
+        let categorypredicate = NSPredicate(format: "parentCategory.name MATCHES %@",selectedCategory!.name!)
+
+        if let addtionPredicate = predicate{
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates:[categorypredicate,addtionPredicate])
+        }else{
+            request.predicate = categorypredicate
+        }
+        
+//        用optional binding寫
+//        let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categorypredicate,predicate!])
+//        request.predicate = compoundPredicate
+        
         do{
-        itemArray = try decoder.decode([Item].self, from: data)
+           itemArray = try context.fetch(request)
         }catch{
-            print("Error decoding item array:\(error.localizedDescription)")
+            print("Error fetching data from context\(error.localizedDescription)")
+        }
+        tableView.reloadData()
+    }
+}
+
+//MARK: - Search Bar methods
+
+extension TodoListViewController:UISearchBarDelegate{
+
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItem()
+            DispatchQueue.main.async {//當收起searchBar.text?.count == 0的時候會把鍵盤收起來
+                searchBar.resignFirstResponder()
+            }
+            
+        }else{//當textDidChange時會立刻搜尋，不用按按鈕
+            let request : NSFetchRequest<Item> = Item.fetchRequest()
+            
+            let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+            
+            request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+            
+            loadItem(with: request, predicate: predicate)
             }
         }
     }
-}
+    
+    
+//
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        if searchBar.text?.count == 0{
+//            loadItem()
+//            tableView.reloadData()
+//            DispatchQueue.main.async {
+//                searchBar.resignFirstResponder()
+//            }
+//        }else{
+//            searchBarSearchButtonClicked(searchBar)
+//        }
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
